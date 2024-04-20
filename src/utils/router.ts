96c1b4c94db3aada0ae8c2e93e4cde95963abb54
Steven Moder <java20131114@gmail.com>
2024-04-21 00:33:31 +0800

    fix: router

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,12 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-  {
-    path: "/",
-    component: () => {
-      import("../views/MainView.vue");
-    },
-  },
+  { path: "/", component: () => import("../views/MainView.vue") },
 ];
 
 const router = createRouter({
