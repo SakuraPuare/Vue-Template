f416dab66984124d4d7ce23de759054d98f5cb30
Steven Moder <java20131114@gmail.com>
2024-04-21 00:00:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1 class="text-6xl text-blue-500">Hello World</h1>
+  </div>
+  <router-view />
+</template>
+
+<style scoped></style>
